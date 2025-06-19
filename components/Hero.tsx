66b39c09103a0cd6aff1b9e8c2b84f950c4c7c23
d-zucho <danny.nico.dev@outlook.com>
2025-06-18 22:59:41 -0700

    add stying to hero + header buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,42 @@
 import MaxWidthWrapper from '@/components/MaxWidthComponent'
 import HeroBadge from '@/components/HeroBadge'
+import { Button } from './ui/button'
+import { ArrowRight } from 'lucide-react'
 
 const Hero = () => {
   return (
-    <section className={'h-[calc(100vh_-_60px)]  bg-[url("/images/hero-bg.png")] w-full bg-cover bg-center overflow-hidden'}>
+    <section
+      className={
+        'h-[calc(100vh_-_60px)]  bg-[url("/images/hero-bg.png")] w-full bg-cover bg-center overflow-hidden'
+      }
+    >
       <MaxWidthWrapper>
         <HeroBadge />
-
-
+        <div className={''}>
+          <div className={'mt-20'}>
+            <h1 className={'flex flex-col text-7xl font-bold'}>
+              <span className={'text-white'}>Unveiling Truth</span>
+              <span className={'text-my-hero-orange'}>Restoring Justice</span>
+            </h1>
+            <p className={'max-w-md mt-5 text-white/60'}>
+              CaseMate brings wrongful conviction data to life—highlighting
+              systemic issues, honoring those affected, and empowering public
+              awareness.
+            </p>
+          </div>
+          <div className='space-x-6'>
+            <Button className='bg-my-orange-500 hover:bg-my-orange-600 text-white mt-10 relative shadow-[inset_-4px_-4px_6px_0px_rgba(0,_0,_0,_0.5)] group hover:shadow-none'>
+              <div className='absolute inset-0 rounded-sm shadow-[inset_4px_5px_6px_0px_rgba(255,_255,_255,_0.2)] group-hover:hidden' />
+              <span>Explore cases</span>
+              <ArrowRight />
+            </Button>
+            <Button className='relative bg-my-btn-gray text-black px-3 py-2 rounded-sm shadow-[inset_-4px_-4px_6px_0px_rgba(0,_0,_0,_0.5)] group hover:shadow-none ring-0 focus:ring-0 outline-none hover:bg-gray-200'>
+              <div className='absolute inset-0 rounded-sm shadow-[inset_4px_5px_6px_0px_rgba(255,_255,_255,_0.7)] group-hover:hidden' />
+              About the project
+              <ArrowRight />
+            </Button>
+          </div>
+        </div>
       </MaxWidthWrapper>
     </section>
   )
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,41 +1,69 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import {NAV_LINKS} from '@/constants'
+import { NAV_LINKS } from '@/constants'
 import MaxWidthWrapper from '@/components/MaxWidthComponent'
 import MobileNav from '@/components/header/MobileNav'
-
+import { Button, buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 const Header = () => {
   return (
-    <header className="w-full max-h-[60px] py-3">
+    <header className='w-full max-h-[60px] py-3'>
       <MaxWidthWrapper>
-        <nav className="flex items-center justify-between">
-
-          <div>
+        <nav className='flex items-center justify-between'>
+          <div className={'flex items-center gap-12'}>
+            {/* logo */}
             <Image
               src={'/images/CM_Logo-dark.svg'}
-              alt="Logo"
+              alt='Logo'
               width={150}
               height={50}
             />
+
+            {/*  Links */}
+            <div className='flex items-center gap-5 max-md:hidden'>
+              {NAV_LINKS.map(
+                ({ label, href }: { label: string; href: string }) => (
+                  <Link key={label} href={href} className={'font-medium'}>
+                    <div className={'w-fit relative overflow-x-clip group'}>
+                      <span className={''}>{label}</span>
+                      <div
+                        className={
+                          'w-full h-0.5 rounded-full bg-my-orange-500 absolute bottom-0 -left-full group-hover:left-0 group-hover:right-0 transition-all duration-400'
+                        }
+                      ></div>
+                    </div>
+                  </Link>
+                )
+              )}
+            </div>
           </div>
 
-          <div className="flex items-center gap-5 max-md:hidden">
-            {NAV_LINKS.map(({label, href}: { label: string, href: string }) => (
-              <Link key={label} href={href} className={'font-medium'}>
-                <div className={'w-fit relative overflow-x-clip group'}>
-
-                  <span className={''}>
-                      {label}</span>
-                  <div className={'w-full h-0.5 rounded-full bg-my-orange-500 absolute bottom-0 -left-full group-hover:left-0 group-hover:right-0 transition-all duration-400'}></div>
-
-                </div>
-              </Link>
-            ))}
+          {/* Buttons */}
+          <div className={'space-x-4'}>
+            <Button
+              className={
+                'bg-my-green-500 hover:bg-my-green-300 transition-colors duration-300 hover:cursor-pointer font-medium  relative shadow-[inset_-4px_-4px_6px_0px_rgba(0,_0,_0,_0.5)] group hover:shadow-none'
+              }
+            >
+              <div className='absolute inset-0 rounded-sm shadow-[inset_4px_5px_6px_0px_rgba(255,_255,_255,_0.2)] group-hover:hidden' />
+              Subscribe
+            </Button>
+            <Button
+              className={cn(
+                'min-w-[100px]',
+                'bg-my-orange-500 hover:bg-my-orange-200 transition-colors duration-300 font-medium border-b-2 relaive shadow-[inset_-4px_-4px_6px_0px_rgba(0,_0,_0,_0.3)] group hover:shadow-none relative'
+              )}
+            >
+              <div className='absolute inset-0 rounded-sm shadow-[inset_4px_5px_6px_0px_rgba(255,_255,_255,_0.2)] group-hover:hidden' />
+              Sign in
+            </Button>
           </div>
-        <div className={'md:hidden'}>
-          <MobileNav />
-        </div>
+
+          {/* Mobile Nav */}
+          <div className={'md:hidden'}>
+            <MobileNav />
+          </div>
         </nav>
       </MaxWidthWrapper>
     </header>
