242dbb2e57ffe94f7d7a7baecd01b9c9dccf1226
D_Zucho <danny.nico.dev@outlook.com>
2025-07-02 00:05:32 -0700

    ui: start news card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Hero from '@/components/Hero'
 import Intro from '@/sections/Intro'
+import NewsCard from '@/components/NewsCard'
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Intro />
+      <NewsCard />
     </div>
   )
 }
