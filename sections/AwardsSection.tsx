b3f08839974d3d451fde78d0e7705f9cda2fbc73
d-zucho <danny.nico.dev@outlook.com>
2025-07-26 02:26:18 -0700

    ui:add award section

--- sections/AwardsSection.tsx
+++ sections/AwardsSection.tsx
@@ -1,5 +1,27 @@
+import MaxWidthWrapper from '@/components/MaxWidthComponent'
+import { AWARD_ICONS } from '@/constants'
+import Image from 'next/image'
+
 const AwardsSection = () => {
-  return <div className='py-20'>AwardsSection</div>
+  return (
+    <section className='py-20'>
+      <MaxWidthWrapper>
+        <div className='flex gap-10 md:gap-20 items-center justify-center flex-wrap'>
+          {AWARD_ICONS.map((award) => (
+            <div key={award.label} className='w-[75px]'>
+              <Image
+                src={award.img}
+                alt={award.label}
+                width={100}
+                height={100}
+                className='object-contain'
+              />
+            </div>
+          ))}
+        </div>
+      </MaxWidthWrapper>
+    </section>
+  )
 }
 
 export default AwardsSection
